83ea9c90e166197834cec9b58b94d51bd8ec052d
Piotr Szymaniec <traf@angrenost.org>
2022-09-08 19:17:03 +0200

    refactor: remove unuseful comment

--- js/modules/sounds.js
+++ js/modules/sounds.js
@@ -4,7 +4,6 @@ export const sfx = {
   gatherTreasure: new Howl({
     src: ["audio/treasure1.mp3", "audio/old-video-game-win.wav"]
   }),
-  //nie udaje mi się odtworzyć dodatkowego dzwięku przy wygranej
   win: new Howl({
     src: ["old-video-game-win.wav"]
   })
